4a91d45c50c686eda574a9630183c0b9198a289c
Hugo <hugo.germain@supelec.fr>
2017-07-11 12:24:34 +0100

    Fixed 360 stream

--- siam_tracker/scripts/360cam_test.py
+++ siam_tracker/scripts/360cam_test.py
@@ -3,21 +3,27 @@ import cv2
 import time
 
 # Garmin VIRB 360 URL
-url = "rtsp://192.168.0.1/livePreviewStream"
+#url = "rtsp://192.168.0.1/livePreviewStream"
 
-cap = cv2.VideoCapture(url)
-print("Opened Stream")
-#time.sleep(5)
+cap = cv2.VideoCapture("../../../../stream.flv")
+print("Opened Stream, waiting for frames...")
+
+# Start at last frame
+count = cap.get(cv2.CAP_PROP_FRAME_COUNT);
+cap.set(cv2.CAP_PROP_POS_FRAMES,count-10);
 
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
-    cv2.waitKey(100)
+    cv2.waitKey(25)
     # Display the resulting frame
-    cv2.imshow('frame',frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    if(ret):
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
+
+print("End")
 # When everything done, release the capture
 cap.release()
 cv2.destroyAllWindows()
